375f0fe7aa11d3a2b2cff13071ef5a4168b5f18f
AlyssaLindsay <lindsay2972@saskpolytech.ca>
2025-04-15 15:19:06 -0600

    commit

--- FE/StellarConsulting/StellarConsulting/obj/Debug/net8.0/StellarConsulting.AssemblyInfo.cs
+++ FE/StellarConsulting/StellarConsulting/obj/Debug/net8.0/StellarConsulting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cecdb4d7e019d0c642e6c7ac0038b75d16e4c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9898a1d414fedc1dba4e1f6d5108f4428c56a8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
